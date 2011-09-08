25e973ddd22aec09a5f0f3cc6a7edb4c90176cdb
Patrick Connolly <patrick.c.connolly@gmail.com>
2011-09-08 16:56:52 -0400

    Initial commit.

---
+++ recipes/default.rb
@@ -0,0 +1,32 @@
+#
+# Cookbook Name:: pdepend
+# Recipe:: default
+#
+# Copyright 2011, Myplanet Digital
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+require_recipe "php"
+
+%w{ pear.pdepend.org }.each do |channel|
+  php_pear_channel channel do
+    action :discover
+  end
+end
+
+php_pear "PHP_Depend" do
+  version node['pdepend']['version']
+  channel "pdepend"
+  action :install
+end
